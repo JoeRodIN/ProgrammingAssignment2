da691fe4a39a6ae510e871a867cf9b9f39e92d62
Joseph Rodriguez <jworkitt@hotmail.com>
2016-02-21 18:19:51 -0500

    Update cachematrix.R
    checked out in R Studio, made changes there and got it working but when i pushed changes it did not appear on GitHub so copying/pasting.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions give the user the ability to cache the inverse of a matrix
+## so when needed subsequently it can just be returned.
 
 ## Write a short comment describing this function
-
+## This function acts as a class for the inverse of a matrix
+## it has set for the invertible matrix 
+## and get functions for both the matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  matInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    matInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) matInverse <<- inv
+  getInverse <- function() matInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
+## This function is called when 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, diag(nrow(data)), ...)
+  x$setInverse(inv)
+  inv
 }
